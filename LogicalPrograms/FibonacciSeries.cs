30b8f077f8c7b5e7aea25908f04c543ce3664386
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-02 15:29:23 +0530

    [Soubarnika]Computation of Fibonacci Series Program

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        public void ReadInput()
+        {
+            Console.WriteLine("Welcome to fibonacci Series");
+            //Reading input from user
+            Console.WriteLine("Enter the nth number:");
+            int seriesLength = Convert.ToInt32(Console.ReadLine());
+            FindFibonacciSeries(seriesLength);
+
+        }
+
+        //method to print fibonacci series
+        private static void FindFibonacciSeries(int fibo)
+        {
+            int fib0 = 0;
+            int fib1 = 1;
+            int fiboNext = 0;
+            Console.Write("{0} {1} ", fib0, fib1);
+            for (int iter = 2; iter < fibo; iter++)
+            {
+                //add perivous two number to print next number
+                fiboNext = fib0 + fib1;
+                Console.Write("{0} ", fiboNext);
+
+                //replace the current number with next number
+                fib0 = fib1;
+                fib1 = fiboNext;
+
+            }
+        }
+
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,9 +4,32 @@ namespace LogicalPrograms
 {
     class Program
     {
+        /// <summary>
+        /// P1 - Printing the fibonacci series
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to logical programming");
+            Console.WriteLine("Choose the program:");
+            Console.WriteLine("1 - Fibonacci Series");
+
+            //Read choice from the user
+            Console.WriteLine("Enter Your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.ReadInput();
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.Read();
         }
     }
 }
