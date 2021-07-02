2354f4fc44a40dd5aed002654ba26a5619ec2c47
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-02 18:21:15 +0530

    [Soubarnika]Computation of Coupon Number Program

---
+++ LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class CouponNumber
+    {
+        private const int TOTAL_COUPON = 10;
+        public static void GenerateCoupon()
+        {
+            //local variable to check number of coupons and array to store distinct coupons
+            int numOfCoupon = 0;
+            int iter = 0;
+            int noRepeat = 1;
+            int generateTime = 0;
+            int[] coupon = new int[10];
+
+            //creating object for random class
+            Random random = new Random();
+
+            //check till total number of distinct coupon is received 
+            while (numOfCoupon < TOTAL_COUPON)
+            {
+                noRepeat = 1;
+                iter = 0;
+                int tempCoupon = random.Next(1, 12);
+                Console.WriteLine("" + tempCoupon);
+
+                while (iter <= numOfCoupon)
+                {
+                    //check distinct or not
+                    if (coupon[iter] == tempCoupon)
+                    {
+                        noRepeat = 0;
+                        break;
+                    }
+                    iter++;
+                }
+                generateTime++;
+                if (noRepeat == 1)
+                {
+                    coupon[numOfCoupon] = tempCoupon;
+                    numOfCoupon++;
+                }
+            }
+            //displays random number needed to generate 10 distinct coupon
+            Console.WriteLine("Number of Time required to generate 10 distict coupon:" + generateTime);
+
+            //displays the distinct coupon
+            Console.WriteLine("The Distinct 10 coupons are :");
+            foreach (int i in coupon)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,58 +4,64 @@ namespace LogicalPrograms
 {
     class Program
     {
-		/// <summary>
-		/// P1 - Printing the fibonacci series
-		/// P2 - Checking the number is perfect number or not
-		/// P3 - Printing the prime number series between  to N
-		/// P4 - Reversing the number
-		/// </summary>
-		/// <param name="args">The arguments.</param>
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Welcome to logical programming");
-			Console.WriteLine("Choose the program:");
-			Console.WriteLine("1 - Fibonacci Series");
-			Console.WriteLine("2 - Perfect Number");
-			Console.WriteLine("3 - Prime Number Series");
-			Console.WriteLine("4 - Reverse the number");
+        /// <summary>
+        /// P1 - Printing the fibonacci series
+        /// P2 - Checking the number is perfect number or not
+        /// P3 - Printing the prime number series between  to N
+        /// P4 - Reversing the number
+        /// P5 - Generating 10 distinct coupon
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programming");
+            Console.WriteLine("Choose the program:");
+            Console.WriteLine("1 - Fibonacci Series");
+            Console.WriteLine("2 - Perfect Number");
+            Console.WriteLine("3 - Prime Number Series");
+            Console.WriteLine("4 - Reverse the number");
+            Console.WriteLine("5 - Generaing Distinct Coupon");
 
-			//Read choice from the user
-			Console.WriteLine("Enter Your choice:");
-			int choice = Convert.ToInt32(Console.ReadLine());
+            //Read choice from the user
+            Console.WriteLine("Enter Your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-			switch (choice)
-			{
-				//creating object for Fibonacci series and invoke the method
-				case 1:
-					FibonacciSeries fibonacciSeries = new FibonacciSeries();
-					fibonacciSeries.ReadInput();
-					break;
+            switch (choice)
+            {
+                //creating object for Fibonacci series and invoke the method
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.ReadInput();
+                    break;
 
-				//creating object for perfect number and invoke the method 
-				case 2:
-					PerfectNumber perfectNumber = new PerfectNumber();
-					perfectNumber.ReadInput();
-					break;
+                //creating object for perfect number and invoke the method 
+                case 2:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    perfectNumber.ReadInput();
+                    break;
 
-				//creating object for prime number and invoke the method 
-				case 3:
-					PrimeNumber prime = new PrimeNumber();
-					prime.ReadInput();
-					break;
+                //creating object for prime number and invoke the method 
+                case 3:
+                    PrimeNumber prime = new PrimeNumber();
+                    prime.ReadInput();
+                    break;
 
-				//creating object for Reverse a number and calling the method 
-				case 4:
-					ReverseNumber reverse = new ReverseNumber();
-					reverse.ReadInput();
-					break;
+                //creating object for Reverse a number and calling the method 
+                case 4:
+                    ReverseNumber reverse = new ReverseNumber();
+                    reverse.ReadInput();
+                    break;
 
-				default:
-					break;
-			}
+                //Generating distinct coupons
+                case 5:
+                    CouponNumber.GenerateCoupon();
+                    break;
 
-			Console.Read();
-		}
-	}
+                default:
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
 }
-
